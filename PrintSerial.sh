e6bb82f9817ba779bcc3aca387135bba9e6bb4c2
e0mi <39672047+e0mi@users.noreply.github.com>
2019-04-11 20:52:43 +0200

    Update PrintSerial.sh

--- PrintSerial.sh
+++ PrintSerial.sh
@@ -1,12 +1,11 @@
 # /bin/bash
-ip="192.168.4.1"
 t="1"
 rep="20"
 
 COUNTER=0
          while [  $COUNTER -lt $rep ]; do
 		stty -F /dev/ttyUSB0 57600
- 		cat /home/e0mi/te > /dev/ttyUSB0
+ 		cat /home/user/te > /dev/ttyUSB0
 		echo -e "\x15n\x15n\x15n \x1B\x6D" > /dev/ttyUSB0
 		
 		
